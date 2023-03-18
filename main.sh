7fb80990a346a21cdf9c59b075d9a7de42a28671
Ward Nakchbandi <hotrod.master@hotmail.com>
2023-03-18 08:20:15 -0400

    first commit

---
+++ main.sh
@@ -0,0 +1,22 @@
+#! /bin/bash
+# Add dependent repositories
+wget -q -O - https://ppa.pika-os.com/key.gpg | sudo apt-key add -
+add-apt-repository https://ppa.pika-os.com
+add-apt-repository ppa:pikaos/pika
+add-apt-repository ppa:kubuntu-ppa/backports
+# Clone Upstream
+git clone https://codeberg.org/fabiscafe/game-devices-udev rules
+cp -rvf rules/*.rules game-devices-udev/usr/lib/udev/rules.d/
+echo -e "fabiscafe-devices ("$(date '+%Y%m%d')".git-99pika"$(date '+%M')") kinetic; urgency=medium\n\n  * New Upstream Release\n\n -- Ward Nakchbandi <hotrod.master@hotmail.com> Sat, 01 Oct 2022 14:50:00 +0200" > game-devices-udev/debian/changelog
+cd game-devices-udev
+
+# Get build deps
+apt-get build-dep ./ -y
+
+# Build package
+dpkg-buildpackage
+
+# Move the debs to output
+cd ../
+mkdir -p ./output
+mv ./*.deb ./output/
