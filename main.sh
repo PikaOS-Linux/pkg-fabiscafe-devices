e3aff05c559581e576202e5212af5f5e32cc46e3
Ward Nakchbandi <hotrod.master@hotmail.com>
2023-03-18 08:30:18 -0400

    first commit

--- main.sh
+++ main.sh
@@ -6,15 +6,16 @@ add-apt-repository ppa:pikaos/pika
 add-apt-repository ppa:kubuntu-ppa/backports
 # Clone Upstream
 git clone https://codeberg.org/fabiscafe/game-devices-udev rules
+mkdir -p ./game-devices-udev/usr/lib/udev/rules.d/
 cp -rvf ./rules/*.rules ./game-devices-udev/usr/lib/udev/rules.d/
 echo -e "game-devices-udev ("$(date '+%Y%m%d')".git-99pika"$(date '+%M')") kinetic; urgency=medium\n\n  * New Upstream Release\n\n -- Ward Nakchbandi <hotrod.master@hotmail.com> Sat, 01 Oct 2022 14:50:00 +0200" > game-devices-udev/debian/changelog
-cd game-devices-udev
+cd ./game-devices-udev
 
 # Get build deps
 apt-get build-dep ./ -y
 
 # Build package
-dpkg-buildpackage
+dpkg-buildpackage --no-sign
 
 # Move the debs to output
 cd ../
