47975ce2e351455ee2ec208a2e6bb8b639162f0a
Ward Nakchbandi (Cosmic Fusion) <83735213+CosmicFusion@users.noreply.github.com>
2023-04-25 19:50:10 +0300

    Update main.sh

--- main.sh
+++ main.sh
@@ -8,7 +8,7 @@ add-apt-repository ppa:kubuntu-ppa/backports
 git clone https://codeberg.org/fabiscafe/game-devices-udev rules
 mkdir -p ./game-devices-udev/etc/udev/rules.d/
 cp -rvf ./rules/*.rules ./game-devices-udev/etc/udev/rules.d/
-echo -e "game-devices-udev ("$(date '+%Y%m%d')".git-99pika2"$(date '+%M')") kinetic; urgency=medium\n\n  * New Upstream Release\n\n -- Ward Nakchbandi <hotrod.master@hotmail.com> Sat, 01 Oct 2022 14:50:00 +0200" > game-devices-udev/debian/changelog
+echo -e "game-devices-udev ("$(date '+%Y%m%d')".git-99pika2"$(date '+%M')").lunar lunar; urgency=medium\n\n  * New Upstream Release\n\n -- Ward Nakchbandi <hotrod.master@hotmail.com> Sat, 01 Oct 2022 14:50:00 +0200" > game-devices-udev/debian/changelog
 cd ./game-devices-udev
 
 # Get build deps
